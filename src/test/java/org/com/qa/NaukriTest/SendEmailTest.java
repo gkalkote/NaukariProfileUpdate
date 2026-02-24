f968a354a00146dad96fab0ba8bc083e18d4b227
Sushant8292 <sushj2405@gmail.com>
2026-02-24 11:07:08 +0530

    Update SendEmailTest.java
    Commented Sushant profile

--- src/test/java/org/com/qa/NaukriTest/SendEmailTest.java
+++ src/test/java/org/com/qa/NaukriTest/SendEmailTest.java
@@ -48,7 +48,7 @@ public class SendEmailTest extends PlaywrightFactoryPage {
             EmailUtils emailUtils = new EmailUtils();
 //            emailUtils.sendEmailToBCC("rajugodamwar@gmail.com", rajuEmailPassKey, HR_Emails, RAJU_EMAIL_SUBJECT, RAJU_EMAIL_BODY, RAJU_RESUME_PATH);
             emailUtils.sendEmailToBCC("yadavpriya73028@gmail.com", priyaEmailPassKey, HR_Emails, PRIYA_EMAIL_SUBJECT, PRIYA_EMAIL_BODY, PRIYA_RESUME_PATH);
-            emailUtils.sendEmailToBCC("sushj2405@gmail.com", sushantEmailPassKey, HR_Emails, SUSHANT_EMAIL_SUBJECT, SUSHANT_EMAIL_BODY, SUSHANT_RESUME_PATH);
+//            emailUtils.sendEmailToBCC("sushj2405@gmail.com", sushantEmailPassKey, HR_Emails, SUSHANT_EMAIL_SUBJECT, SUSHANT_EMAIL_BODY, SUSHANT_RESUME_PATH);
             emailUtils.sendEmailToBCC("aniketpotdar88@gmail.com", aniketaEmailPassKey, HR_Emails, ANIKET_EMAIL_SUBJECT, ANIKET_EMAIL_BODY, ANIKET_RESUME_PATH);
             emailUtils.sendEmailToBCC("anchalsingh1029@gmail.com", anchalEmailPassKey, HR_Emails, ANCHAL_EMAIL_SUBJECT, ANCHAL_EMAIL_BODY, ANCHAL_RESUME_PATH);
             emailUtils.sendEmailToBCC("truptirahir26@gmail.com", truptiEmailPassKey, HR_Emails, TRUPTI_EMAIL_SUBJECT, TRUPTI_EMAIL_BODY, TRUPTI_RESUME_PATH);
